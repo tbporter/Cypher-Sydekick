46989478f9360b1d7f681e79caf37383ecb9fddd
Alex Mosolgo <amosolgo@gmail.com>
2013-11-12 22:36:03 -0500

    Created dummy listeners for NFCActivity buttons.

--- CypherSydekick/src/com/github/tbporter/cypher_sydekick/NFCActivity.java
+++ CypherSydekick/src/com/github/tbporter/cypher_sydekick/NFCActivity.java
@@ -3,8 +3,11 @@ package com.github.tbporter.cypher_sydekick;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class NFCActivity extends Activity {
 	private EditText m_nfcEditText;
@@ -20,6 +23,30 @@ public class NFCActivity extends Activity {
         m_nfcEditText = (EditText) findViewById(R.id.nfcEditText);
         m_nfcBroadcastButton = (Button) findViewById(R.id.nfcBroadcastButton);
         m_nfcReceiveButton = (Button) findViewById(R.id.nfcReceiveButton);
+        
+        // Create listener for broadcast button
+        m_nfcBroadcastButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				broadcastButtonClicked();
+			}
+		});
+        
+        // Create listener for receive button
+        m_nfcReceiveButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				receiveButtonClicked();
+			}
+		});
+    }
+    
+    private void broadcastButtonClicked() {
+    	Toast.makeText(this, "Broadcast button clicked, text:\n" + m_nfcEditText.getText(), Toast.LENGTH_SHORT).show();
+    }
+    
+    private void receiveButtonClicked() {
+    	Toast.makeText(this, "Receive button clicked", Toast.LENGTH_SHORT).show();
     }
 
 
