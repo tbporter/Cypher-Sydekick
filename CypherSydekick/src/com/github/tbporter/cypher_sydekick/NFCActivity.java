9bee5458bbb55efcb29ab8ce6bad29e2d07163d4
Alex Mosolgo <amosolgo@gmail.com>
2013-11-12 21:39:05 -0500

    Added NFCActivity UI elements.

--- CypherSydekick/src/com/github/tbporter/cypher_sydekick/NFCActivity.java
+++ CypherSydekick/src/com/github/tbporter/cypher_sydekick/NFCActivity.java
@@ -1,15 +1,25 @@
 package com.github.tbporter.cypher_sydekick;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NFCActivity extends Activity {
+	private EditText m_nfcEditText;
+	private Button m_nfcBroadcastButton;
+	private Button m_nfcReceiveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+        
+        // Get views by id
+        m_nfcEditText = (EditText) findViewById(R.id.nfcEditText);
+        m_nfcBroadcastButton = (Button) findViewById(R.id.nfcBroadcastButton);
+        m_nfcReceiveButton = (Button) findViewById(R.id.nfcReceiveButton);
     }
 
 
