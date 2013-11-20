4d5769e4831b42b80046bade17aa904a7ea7d4fb
Alex Mosolgo <amosolgo@gmail.com>
2013-11-20 13:53:01 -0500

    Updated MessagesServlet in preparation for demo.

--- Server/CypherSydekick Server/src/com/github/tbporter/cypher_sydekick/servlets/MessagesServlet.java
+++ Server/CypherSydekick Server/src/com/github/tbporter/cypher_sydekick/servlets/MessagesServlet.java
@@ -23,6 +23,12 @@ public class MessagesServlet extends HttpServlet {
 	private static final String PARAM_ACTION = "action";
 	private static final String PARAM_RECIPIENT = "recipient";
 	private static final String PARAM_SENDER = "sender";
+	private static final String PARAM_MESSAGE = "message";
+
+	/** Action for sending a message. */
+	private static final String ACTION_SEND = "send";
+	/** Action for receiving a message. */
+	private static final String ACTION_RECEIVE = "receive";
 
 	/** Default error code sent for general errors. */
 	private static final int STATUS_ERROR = HttpStatus.INTERNAL_SERVER_ERROR_500;
@@ -45,34 +51,53 @@ public class MessagesServlet extends HttpServlet {
 		final String action = req.getParameter(PARAM_ACTION);
 		final String sender = req.getParameter(PARAM_SENDER);
 		final String recipient = req.getParameter(PARAM_RECIPIENT);
-
-		boolean validParams = true; // Assume params are valid first
+		final String message = req.getParameter(PARAM_MESSAGE);
 
 		// Exception handling for validating sender and recipient
 		try {
-			if (DatabaseManager.getUser(sender) == null) {
-				out.write("Sender " + sender + " does not exist.");
-				validParams = false;
-			}
-			if (DatabaseManager.getUser(recipient) == null) {
-				out.write("Recipient " + recipient + " does not exist.");
-				validParams = false;
-			}
-		} catch (DatabaseManagerException e) {
-			out.write("Internal database error.");
-		}
 
-		// Make sure parameters were provided
-		if (validParams) {
-			if (action.equals("send")) {
+			// Make sure sender exists
+			if (DatabaseManager.getUser(sender) != null) {
 
-			} else if (action.equals("receive")) {
+				// Make sure recipient exists
+				if (DatabaseManager.getUser(recipient) != null) {
+
+					// Check the provided action
+					switch (action) {
+					case ACTION_SEND:
+						// Replace this placeholder with the actual send method,
+						// something like:
+						// sendMessage(sender, recipient, message)
+						out.write("Sending a message from " + sender + " to "
+								+ recipient + ".  Message contents:\n"
+								+ message);
+						resp.setStatus(STATUS_SUCCESS);
+						break;
+
+					case ACTION_RECEIVE:
+						// Replace this placeholder with the actual receive
+						// method, something like:
+						// String message = receiveMessage(sender, recipient)
+						out.write("Receiving a message from " + sender + " to "
+								+ recipient + ".");
+						resp.setStatus(STATUS_SUCCESS);
+						break;
+
+					default:
+						out.write("Invalid action.");
+						break;
+					}
+
+				} else {
+					out.write("Recipient " + recipient + " does not exist.");
+				}
 
 			} else {
-				out.write("Invalid action.");
+				out.write("Sender " + sender + " does not exist.");
 			}
-		} else {
-			out.write("Invalid parameters.");
+
+		} catch (DatabaseManagerException e) {
+			out.write("Internal database error.");
 		}
 	}
 
