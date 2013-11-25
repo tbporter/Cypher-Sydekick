21effcce9c3f25c8a6cb74b946bfd1875879530d
Alex Mosolgo <amosolgo@gmail.com>
2013-11-24 21:58:56 -0800

    Copied NFC funcitonality from NFCActivity to ChatClientActivity.

--- CypherSydekick/src/com/github/tbporter/cypher_sydekick/activities/ChatClientActivity.java
+++ CypherSydekick/src/com/github/tbporter/cypher_sydekick/activities/ChatClientActivity.java
@@ -1,12 +1,11 @@
 package com.github.tbporter.cypher_sydekick.activities;
 
-import com.github.tbporter.cypher_sydekick.R;
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +19,12 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.github.tbporter.cypher_sydekick.R;
+import com.github.tbporter.cypher_sydekick.nfc.NFCManager;
+import com.github.tbporter.cypher_sydekick.nfc.NFCManagerException;
+import com.github.tbporter.cypher_sydekick.users.UserInfo;
 
 public class ChatClientActivity extends Activity {
 
@@ -32,6 +37,9 @@ public class ChatClientActivity extends Activity {
 	private String[] mFriendsArray;
 
 	private ChatFragment chatFragment_ = new ChatFragment();
+
+	/** NFCManager to handle NFC operations. */
+	private NFCManager m_nfcManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -85,9 +93,85 @@ public class ChatClientActivity extends Activity {
 		};
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
+		// Create the NFCManager
+		try {
+			m_nfcManager = new NFCManager(NfcAdapter.getDefaultAdapter(this),
+					this);
+		} catch (NFCManagerException nme) {
+			Toast.makeText(this, nme.getMessage(), Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+
+		// Set the active user whose info should be shared over NFC
+		// TODO: Send real user information
+		m_nfcManager.setCurrentUser(new UserInfo("username", "key"));
+
+		// Notify the user if they have not enabled NFC
+		checkAndNotifyNFCEnabled();
+
 		/*
 		 * if (savedInstanceState == null) { selectItem(0); }
 		 */
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		// Stop listening for NFC messages
+		m_nfcManager.stopNFCReceive();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Start listening for NFC messages
+		checkAndNotifyNFCEnabled();
+		m_nfcManager.startNFCReceive();
+	}
+
+	/**
+	 * Checks if NFC is enabled and notifies the user if it is not.
+	 */
+	private void checkAndNotifyNFCEnabled() {
+		if (!m_nfcManager.isNFCEnabled()) {
+			Toast.makeText(this,
+					"NFC disabled, must enable before Android Beam will work.",
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	/**
+	 * Processes intent for a completed NFC read.
+	 */
+	@Override
+	public void onNewIntent(final Intent intent) {
+		super.onNewIntent(intent);
+
+		// Pass the intent to the NFCManager and see if it handles it
+		if (m_nfcManager.willHandleIntent(intent)) {
+			// Parse the intent with the NFCManager
+			final UserInfo receivedUser = m_nfcManager.handleIntent(intent);
+
+			// Make sure the UserInfo was parsed successfully
+			if (null != receivedUser) {
+				Toast.makeText(
+						this,
+						"Received user information:\n"
+								+ receivedUser.toString(), Toast.LENGTH_LONG)
+						.show();
+			} else {
+				Toast.makeText(this, "Error receiving user information",
+						Toast.LENGTH_LONG).show();
+			}
+
+		}
+		// If the NFCManager doesn't handle this intent, do nothing with it
+		else {
+		}
+
 	}
 
 	@Override
