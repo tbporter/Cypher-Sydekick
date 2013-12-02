c092115f84c8c769210d0da833b33989701ee3fa
Alex Mosolgo <amosolgo@gmail.com>
2013-12-02 18:41:38 -0500

    Completed implementation of sort-of-secure hashing for reset password checking.

--- Server/CypherSydekick Server/src/com/github/tbporter/cypher_sydekick/servlets/ResetServlet.java
+++ Server/CypherSydekick Server/src/com/github/tbporter/cypher_sydekick/servlets/ResetServlet.java
@@ -2,7 +2,12 @@ package com.github.tbporter.cypher_sydekick.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +20,7 @@ import com.github.tbporter.cypher_sydekick.debugging.Debug;
 
 public class ResetServlet extends HttpServlet {
 	private static final String TAG = "ResetServlet";
-	
+
 	/** Parameter key for reset password. */
 	private static final String PARAM_PASSWORD = "password";
 
@@ -23,6 +28,17 @@ public class ResetServlet extends HttpServlet {
 	private static final int STATUS_ERROR = HttpStatus.INTERNAL_SERVER_ERROR_500;
 	/** Default success code sent for seccessful operations. */
 	private static final int STATUS_SUCCESS = HttpStatus.OK_200;
+
+	/** Dummy salt for password hashing. */
+	private static final byte[] PASSWORD_SALT = new byte[] { 0 };
+	/** Iteration count for password hashing. */
+	private static final int PASSWORD_ITERATIONS = 2;
+	/** Key size for password hashing. */
+	private static final int PASSWORD_KEYLENGTH = 256;
+	/** Hashed password to reset the database. */
+	private static final byte[] PASSWORD = new byte[] { 44, 91, -30, -84, 67,
+			-37, -116, 68, -12, -54, 53, -36, 83, -7, -39, 48, 3, 12, -105,
+			-11, 9, 51, 39, 105, 61, -18, 33, 101, 89, 36, 25, -61 };
 
 	/**
 	 * Accepts new users based on parameters provided with the request.
@@ -50,11 +66,11 @@ public class ResetServlet extends HttpServlet {
 					Debug.printMsg(TAG, "Created users table.");
 					DatabaseManager.createMessagesTable();
 					Debug.printMsg(TAG, "Created messages table.");
-					
+
 					// Indicate success
 					resp.setStatus(STATUS_SUCCESS);
 					out.write("Database reset.");
-					
+
 				} catch (DatabaseManagerException e) {
 					out.write("Internal database error.");
 				}
@@ -85,9 +101,38 @@ public class ResetServlet extends HttpServlet {
 	 * @return <tt>true</tt> if the password is valid, <tt>false</tt> if not.
 	 */
 	private boolean passwordValid(final String password) {
-		boolean retVal = true;
+		boolean retVal = false;
+
+		if ((null != password) && (!password.isEmpty())) {
+			try {
+				// Create a key object using the entered password and the
+				// selected parameters
+				final PBEKeySpec keySpec = new PBEKeySpec(
+						password.toCharArray(), PASSWORD_SALT,
+						PASSWORD_ITERATIONS, PASSWORD_KEYLENGTH);
+
+				// Generate the hash for the entered password
+				final SecretKeyFactory keyFactory = SecretKeyFactory
+						.getInstance("PBKDF2WithHmacSHA1");
+				final SecretKey key = keyFactory.generateSecret(keySpec);
+				final byte[] encoded = key.getEncoded();
+
+				// Print the hash of the entered password
+				final String encodedString = Arrays.toString(encoded);
+				Debug.printMsg(TAG, "Entered password hash size = "
+						+ encoded.length + ", contents:\n" + encodedString);
+
+				// Check the hash against the stored hash
+				retVal = Arrays.equals(encoded, PASSWORD);
+
+			} catch (Exception e) {
+				// Log this and let the password check fail
+				final String exceptionType = e.getClass().getName();
+				Debug.printError(TAG,
+						exceptionType + " in passwordValid: " + e.getMessage());
+			}
+		}
 
 		return retVal;
 	}
-
 }
