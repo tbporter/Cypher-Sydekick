af5a461baf8bc49e8e86496f4f8c6f985d10830b
tbporter <travis.porter@gmail.com>
2013-12-02 17:15:07 -0500

    Added shooter AI

---
+++ CypherSydekick/src/com/bls220/cyphersidekick/entities/ai/Shooter.java
@@ -0,0 +1,62 @@
+package com.bls220.cyphersidekick.entities.ai;
+
+import com.bls220.cyphersidekick.entities.Entity;
+import com.bls220.cyphersidekick.entities.Player;
+import com.bls220.cyphersidekick.screens.MainScreen;
+
+
+public class Shooter extends AI {
+	enum States { idle, chasing, running, standing};
+	States mState;
+	private static final double DETECT_DIST = 7; //entity inactive until player is within this range
+	private static final double STOP_CHASE_DIST = 10; //entity will stop chasing player
+	private static final double SAFE_DIST = 3; //entity will try to keep this distance
+	private static final double SAFE_DIST_JITTER = .3; //so the entity doesn't have to be exactly precise
+	
+	Shooter(Entity e) {
+		super(e);
+		mState = States.idle;
+	}
+	
+	@Override
+	public void update() {
+		Player p = MainScreen.mPlayer;
+		Double dist = Entity.getDist(mEntity, p);
+		switch(mState){
+		case idle:
+			if(dist <= DETECT_DIST){
+				mState = States.chasing;
+			}
+			break;
+		case chasing:
+			if(dist >= STOP_CHASE_DIST)
+				mState = States.idle;
+			else if(dist <= SAFE_DIST)
+				mState = States.running;
+			else if(dist >= SAFE_DIST && dist <= SAFE_DIST+ SAFE_DIST_JITTER)
+				mState = States.standing;
+			else{
+				mEntity.setHeading(p.getX() - mEntity.getX(),p.getY() - mEntity.getY());
+			}
+			break;
+		case running:
+			if(dist >= SAFE_DIST)
+				mState = States.chasing;
+			else{
+				mEntity.setHeading(mEntity.getX() - p.getX(), mEntity.getY() - p.getY());
+			}
+			break;
+		case standing:
+			if(dist <= SAFE_DIST)
+				mState = States.running;
+			else if(dist >= SAFE_DIST + SAFE_DIST_JITTER)
+				mState = States.chasing;
+			else{
+				
+			}
+			break;
+		}
+
+	}
+
+}
