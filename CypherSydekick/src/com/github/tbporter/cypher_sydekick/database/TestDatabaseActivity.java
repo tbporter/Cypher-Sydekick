e2cb659c9b21e8f37c7d367414d63572feb22036
woonkill <chickendance2@hotmail.com>
2013-12-02 19:49:20 -0500

    Database framework implemented and new test activity made to test the database.

---
+++ CypherSydekick/src/com/github/tbporter/cypher_sydekick/database/TestDatabaseActivity.java
@@ -0,0 +1,69 @@
+package com.github.tbporter.cypher_sydekick.database;
+
+import java.util.List;
+import java.util.Random;
+
+import com.github.tbporter.cypher_sydekick.R;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class TestDatabaseActivity extends ListActivity {
+  private UserKeyDOA datasource;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.database_client);
+
+    datasource = new UserKeyDOA(this);
+    datasource.open();
+
+    List<String> values = datasource.getAllUsers();
+
+    // use the SimpleCursorAdapter to show the
+    // elements in a ListView
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, values);
+    setListAdapter(adapter);
+  }
+
+  // Will be called via the onClick attribute
+  // of the buttons in main.xml
+  public void onClick(View view) {
+    @SuppressWarnings("unchecked")
+    ArrayAdapter<UserKey> adapter = (ArrayAdapter<UserKey>) getListAdapter();
+    UserKey username = null;
+    switch (view.getId()) {
+    case R.id.add:
+      String[] usernames = new String[] { "Ben", "Travis", "Alex" };
+      int nextInt = new Random().nextInt(3);
+      // save the new users to the database
+      username = datasource.createUser(usernames[nextInt]);
+      adapter.add(username);
+      break;
+    case R.id.delete:
+      if (getListAdapter().getCount() > 0) {
+    	username = (UserKey) getListAdapter().getItem(0);
+        datasource.deleteUser(username);
+        adapter.remove(username);
+      }
+      break;
+    }
+    adapter.notifyDataSetChanged();
+  }
+
+  @Override
+  protected void onResume() {
+    datasource.open();
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    datasource.close();
+    super.onPause();
+  }
+}
--- CypherSydekick/src/com/github/tbporter/cypher_sydekick/database/UserKeyDOA.java
+++ CypherSydekick/src/com/github/tbporter/cypher_sydekick/database/UserKeyDOA.java
@@ -1,6 +1,8 @@
 package com.github.tbporter.cypher_sydekick.database;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -8,5 +10,61 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class UserKeyDOA {
-
+	private SQLiteDatabase database;
+	private UserKeyDatabaseHelper dbHelper;
+	private String[] allColumns = { UserKeyDatabaseHelper.COLUMN_ID, UserKeyDatabaseHelper.COLUMN_USERNAME };
+	
+	public UserKeyDOA(Context context) {
+		dbHelper = new UserKeyDatabaseHelper(context);
+	}
+	
+	public void open() throws SQLException {
+		database = dbHelper.getWritableDatabase();
+	}
+	
+	public void close() {
+		dbHelper.close();
+	}
+	
+	public UserKey createUser(String comment) {
+		ContentValues values = new ContentValues();
+		values.put(UserKeyDatabaseHelper.COLUMN_USERNAME, comment);
+		long insertId = database.insert(UserKeyDatabaseHelper.TABLE_KEYS, null, values);
+		Cursor cursor = database.query(UserKeyDatabaseHelper.TABLE_KEYS, allColumns,
+				UserKeyDatabaseHelper.COLUMN_ID + " = " + insertId, null, null, null, null);
+		cursor.moveToFirst();
+		UserKey newComment = cursorToUsername(cursor);
+		cursor.close();
+		return newComment;
+	}
+	
+	public void deleteUser(UserKey username) {
+		long id = username.getId();
+		System.out.println("Username deleted with id: " + id);
+		database.delete(UserKeyDatabaseHelper.TABLE_KEYS, UserKeyDatabaseHelper.COLUMN_ID + " = " + id, null);
+	}
+	
+	 public List<String> getAllUsers() {
+	   List<String> usernames = new ArrayList<String>();
+	
+		Cursor cursor = database.query(UserKeyDatabaseHelper.TABLE_KEYS, allColumns,
+				null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			UserKey username = cursorToUsername(cursor);
+			usernames.add(username.getUsername());
+			cursor.moveToNext();
+		}
+		// make sure to close the cursor
+		cursor.close();
+		return usernames;
+	}
+	
+	private UserKey cursorToUsername(Cursor cursor) {
+		UserKey username = new UserKey();
+		username.setId(cursor.getLong(0));
+		username.setUsername(cursor.getString(1));
+		return username;
+	}
 }
